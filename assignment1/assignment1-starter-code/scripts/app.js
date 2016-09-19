aeb489ae979783236403c74cb6119c0b2c607fc8
David <dave.leslie.online@gmail.com>
2016-09-19 20:58:06 +0200

    ignore extra commas and remove debug code

--- assignment1/assignment1-starter-code/scripts/app.js
+++ assignment1/assignment1-starter-code/scripts/app.js
@@ -2,39 +2,37 @@
 'use strict';
 
 angular.module('ass1App', [])
+    .controller('LunchCheckController', function($scope) {
 
-  .controller('LunchCheckController', function($scope) {
+        $scope.lunch = "";
 
-    $scope.lunch = "";
+        $scope.lunchCheck = function() {
+        //var lunchResult = checkLunch($scope.lunch);
+        $scope.lunchMessage = checkLunch($scope.lunch)
 
-    $scope.lunchCheck = function() {
-      var lunchResult = checkLunch($scope.lunch);
-      $scope.lunchMessage = lunchResult;
-
-    };
+        };
 
     function checkLunch(string) {
-      $scope.lunchMenuArr = $scope.lunch.split(',');
-      $scope.menuSize = $scope.lunchMenuArr.length - 1;
-      var lunchCheckResult = "";
+        var lunchMenuArr = $scope.lunch.split(',');
+        var cleanArr = [];
+        for (var i = 0; i < lunchMenuArr.length; i++ ) {
+            if (!(lunchMenuArr[i].length == 0)) {
+                cleanArr.push(lunchMenuArr[i]);
+            }
+        }
+        var menuSize = cleanArr.length;
+        var lunchCheckResult = "";
+        if (menuSize == 0) {
+            lunchCheckResult = "Please enter data first";
+        }   else if (menuSize <= 3) {
+            lunchCheckResult = "Enjoy!";
+        }   else {
+            lunchCheckResult = "Too much!";
+        }
 
-      if ($scope.menuSize == 0) {
-        lunchCheckResult = "Please enter data first";
-      } else if ($scope.menuSize <= 2) {
-        lunchCheckResult = "Enjoy!";
-      } else {
-        lunchCheckResult = "Too much!";
-      }
-
-      return lunchCheckResult;
-    }
-
-
+            return lunchCheckResult;
+        }
 
   });
-
-
-
-
 
 })();
