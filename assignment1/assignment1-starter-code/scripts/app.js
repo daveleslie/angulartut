aeb6fb075cba19e93d89207035a436679d2fcfb7
David <dave.leslie.online@gmail.com>
2016-09-21 11:11:25 +0200

    protect from minification

--- assignment1/assignment1-starter-code/scripts/app.js
+++ assignment1/assignment1-starter-code/scripts/app.js
@@ -1,38 +1,37 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('ass1App', [])
-    .controller('LunchCheckController', function($scope) {
+    angular.module('ass1App', [])
+        .controller('LunchCheckController', LunchCheckController);
 
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
         $scope.lunch = "";
-
         $scope.lunchCheck = function() {
-        //var lunchResult = checkLunch($scope.lunch);
-        $scope.lunchMessage = checkLunch($scope.lunch)
+            //var lunchResult = checkLunch($scope.lunch);
+            $scope.lunchMessage = checkLunch($scope.lunch)
 
         };
 
-    function checkLunch(string) {
-        var lunchMenuArr = $scope.lunch.split(',');
-        var cleanArr = [];
-        for (var i = 0; i < lunchMenuArr.length; i++ ) {
-            if (!(lunchMenuArr[i].length == 0)) {
-                cleanArr.push(lunchMenuArr[i]);
+        function checkLunch(string) {
+            var lunchMenuArr = $scope.lunch.split(',');
+            var cleanArr = [];
+            for (var i = 0; i < lunchMenuArr.length; i++ ) {
+                if (!(lunchMenuArr[i].length == 0)) {
+                    cleanArr.push(lunchMenuArr[i]);
+                }
             }
-        }
-        var menuSize = cleanArr.length;
-        var lunchCheckResult = "";
-        if (menuSize == 0) {
-            lunchCheckResult = "Please enter data first";
-        }   else if (menuSize <= 3) {
-            lunchCheckResult = "Enjoy!";
-        }   else {
-            lunchCheckResult = "Too much!";
-        }
-
+            var menuSize = cleanArr.length;
+            var lunchCheckResult = "";
+            if (menuSize == 0) {
+                lunchCheckResult = "Please enter data first";
+            }   else if (menuSize <= 3) {
+                lunchCheckResult = "Enjoy!";
+            }   else {
+                lunchCheckResult = "Too much!";
+            }
             return lunchCheckResult;
         }
-
-  });
+    }
 
 })();
